b3640525f7bdc44c51d63053ed28e84baf4437a3
someear9h <tikotkarsamarth@gmail.com>
2025-09-08 21:15:03 +0530

    changed vite frontend url and connected to backend

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -4,4 +4,4 @@
 // the app is running locally or deployed on Choreo.
 export const BASE_URL = window.location.hostname === 'localhost'
   ? ''  // Uses the proxy in vite.config.js for local development
-  : '/choreo-apis/project-setu-internal-dem/backend/v1.0'; // Your Choreo Invoke URL
+  : "/choreo-apis/project-setu-internal-dem/backend/v1"; // Your Choreo Invoke URL
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-      },
-    },
-  },
+// https://vite.dev/config/
+export default defineConfig((command, mode) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  console.log(env.VITE_DEBUG)
+
+  return {
+    plugins: [react()],
+    server: {
+    ...(env.VITE_DEBUG === "true" && {
+      proxy: {
+        "/api": {
+          target: "http://localhost:8000",
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    }
+    )}
+  }
 })
