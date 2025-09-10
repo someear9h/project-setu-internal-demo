e5440f14f7cfcd2d5534f19249ea246619bd9c17
someear9h <tikotkarsamarth@gmail.com>
2025-09-10 12:50:54 +0530

    changed invoke url for choreo

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -4,4 +4,4 @@
 // the app is running locally or deployed on Choreo.
 export const BASE_URL = window.location.hostname === 'localhost'
   ? '/api'  // Uses the proxy in vite.config.js for local development
-  : "/choreo-apis/project-setu-internal-dem/backend/v1/api"; // Your Choreo Invoke URL
+  : "/choreo-apis/project-setu-internal-dem/backend/v1/"; // Your Choreo Invoke URL
