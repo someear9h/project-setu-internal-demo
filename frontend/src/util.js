6e7a037a0955482bb9224867a8f9757f61d5c133
someear9h <tikotkarsamarth@gmail.com>
2025-09-11 22:01:22 +0530

    added render url in frontend

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -2,4 +2,6 @@
 
 // This logic dynamically sets the backend URL based on whether
 // the app is running locally or deployed on Choreo.
-export const BASE_URL = '/api'
+export const BASE_URL = window.location.hostname === 'localhost'
+  ? '/api'  // Uses the proxy in vite.config.js for local development
+  : "https://project-setu-internal-demo.onrender.com/api";
